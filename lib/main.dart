bf420ba1975493ba0e55c1fdf82c7679b4cbbf55
franciscogneto <francisco_gneto@hotmail.com>
2021-04-22 18:14:11 -0300

    alteração para executar o firebase no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:incidencias_app/pages/menu_page.dart';
+import 'package:loading/indicator/ball_pulse_indicator.dart';
 import 'pages/login_page.dart';
-import 'pages/menu_page.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:incidencias_app/pages/menu_page.dart';
+import 'package:loading/loading.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  String _email, _password;
+  final Future<FirebaseApp> _fApp = Firebase.initializeApp();
+  //final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: FutureBuilder(
+        future: _fApp,
+        builder: (context,snapshot) {
+          if(snapshot.hasError){
+            return Text('Erro');
+          }else if(snapshot.hasData) {
+            return LoginPage();
+          } else {
+            return Center(
+              child: Loading(indicator: BallPulseIndicator(), size: 100.0,color: Colors.indigo),
+            );
+          }
+        }
+      ),
+      //LoginPage(),
+    );
+  }
+}
+
+/*class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,5 +64,5 @@ class MyApp extends StatelessWidget {
       home: LoginPage(),
     );
   }
-}
+}*/
 
